7a4cedad39139d6f40e590ae159447bdebcbd23c
Stijn Haezebrouck <stijn@haezebrouck.be>
2017-02-06 06:34:15 +0100

    Container wonderfall-wait-for-db now waits for database first. An environment parameter  was added

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo "Waiting for mysql..."
+echo "Waiting for database on $DB_HOST:$DB_PORT..."
 
-while ! nc -z nextcloud-db 3306 > /dev/null 2>&1; do
+while ! nc -z $DB_HOST $DB_PORT > /dev/null 2>&1; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
 
-echo "Mysql was started"
+echo "Database is available"
+
+$1
